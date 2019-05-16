f1ea71a8fbdf8f1b89403e1e09a7a5f18452b853
Nursaid Saidmakhmudov <uzbltd@Nursaids-MacBook-Pro.local>
2019-05-16 16:26:25 -0700

    added data to tables

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+USE bamazon;
+INSERT INTO products
+(product_name, department_name, price, stock_quantity)
+VALUES
+ ("iponeX case", "Phone Accessories", 10.50,80),
+ ("Sneakers", "Shoes", 80.99, 50),
+ ("Tool-Set", "Household", 20.99, 450),
+ ("Key chain", "Accessories", 2.99, 100),
+ ("Textbook", "Office", 2.99, 1000),
+ ("Slipers", "Shoes", 10.99, 200),
+ ("Headphones", "Acessories", 15.99, 100),
+ ("Batteries", "Accessories", 5.99, 150),
+ ("Pen", "Office", 1.99, 100),
+ ("Marker", "Office", 0.99, 100);
+ 
+ 
