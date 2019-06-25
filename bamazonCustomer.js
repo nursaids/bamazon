5a62642eb63e73a837bf5284ab72c02ff1987a5a
nursaid <uznet@yahoo.com>
2019-06-25 11:53:19 -0700

    changed js file and deleted files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,54 +1,191 @@
-require("dotenv").config();
+
 
 
 const mysql = reqiure("mysql");
-const Table = require("cli-table");
+const Table = require("cli-table3");
 const inquirer = require("inquirer");
-const keys = require("./keys.js");
-const Bamazon = require("./bamazonDB.js");
+
 
 
 const connection = mysql.createConnection({
     host:"localhost",
     port: 3306,
     user:"root",
-    password: keys.rootPassword,
-    database:"bamazon"
+    password: "Be2ge4r713$",
+    database:"bamazonDB"
 
 
 });
 
-let bamazon = new Bamazon (connection);
 
-connection.connect(function(err){
+
+connection.connect(function (err) {
     if (err) throw err;
-
-    displayProducts();
-
-
-});
-
-function displayProducts(){
-
-    bamazon.selectAllFrom("products", function(res){
-    
-        let table = new Table({
-            head:["ID", "Product", "Department", "Price", "Quantity"],
-            colAligns: ["right", null, null, "right", "right"]
-        });
-
-        res.forEach(function(product){
-            tableRow = [];
-            for (let key in product){
-                tableRow.push(product[key]);
+  
+    start()
+  });
+  
+  function start() {
+    inquirer.prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+  
+      choices: [
+        "See the list of products",
+        "Buy a product by ID",
+        "exit"
+      ]
+    })
+      .then(function (answer) {
+        switch (answer.action) {
+          case "See the list of products":
+            displayProducts();
+            break;
+  
+  
+          case "Buy a product by ID":
+            runUserBuy();
+            break;
+  
+          case "exit":
+            connection.end();
+            break;
         }
-        
-        table.push(tableRow);
-
+      });
+  }
+  
+  function secondMenu() {
+    inquirer.prompt({
+      name: "action",
+      type: "confirm",
+      message: "Would you like to buy a product?",
+    }).then(function (answer) {
+      // console.log(answer);
+  
+      if (answer.action == true) {
+        runUserBuy();
+      } else {
+        console.log("Thank you!!");
+        connection.end();
+      }
     });
-    console.log(table.toString());
-    promptPurchase()
-
-});
-}
-
+  
+  }
+  
+  function displayProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+      if (err) throw err;
+      // Cli-Table display code with Color
+      var table = new Table(
+        {
+          head: ["ID", "Product Name", "Department Name", "Quantity", "Price"],
+          colWidths: [5, 40, 20, 12, 12],
+        });
+  
+      // Set/Style table headings and Loop through entire inventory
+      for (var i = 0; i < res.length - 1; i++) {
+        table.push(
+          [res[i].item_id, res[i].product_name, res[i].department_name, res[i].stock_quantity, res[i].price]
+        );
+      }
+      console.log(table.toString());
+      secondMenu();
+  
+    });
+  
+  
+  }
+  
+  
+  
+  function runUserBuy() {
+    inquirer
+      .prompt([
+        {
+          name: "id",
+          type: "input",
+          message: "What is the product ID you want to buy?",
+          validate: function (value) {
+            if (isNaN(value) === false && !value == "") {
+              return true;
+            }
+            console.log("Please enter a number");
+            return false;
+          }
+        },
+        {
+          name: "units",
+          type: "input",
+          message: "How many units of the product would you like to buy?: ",
+          validate: function (value) {
+            if (isNaN(value) === false && !value == "") {
+  
+               return true;
+  
+            }
+           
+            console.log("Please introduce a number");
+            
+            return false;
+  
+          }
+        }
+      ]).then(function (answer) {
+        // console.log(answer.id);
+        // console.log(answer.units)
+  
+        var query = "SELECT item_id, product_name, department_name, price, stock_quantity from products WHERE ?";
+        connection.query(query, { item_id: answer.id }, function (err, res) {
+  
+          for (var i = 0; i < res.length; i++) {
+  
+            // var selectedItem = res[i].item_id;
+            // console.log(selectedItem);
+            var stockAvaible = (res[i].stock_quantity);
+            console.log(stockAvaible);
+            console.log("The product you have selected is: " + res[i].product_name + ". with the ID number: " + res[i].item_id);
+  
+            // Case escenario 1: units less or equal to our inventory
+            if (answer.units <= stockAvaible) {
+  
+              var updateStock = connection.query(
+                "UPDATE products SET ? WHERE ?",
+                [
+                  {
+                    stock_quantity: ((res[i].stock_quantity) - answer.units)
+                  },
+                  {
+                    item_id: res[i].item_id
+                  }
+                ],
+                function (err, res) {
+                  // console.log(res.affectedRows );
+  
+  
+                }
+              );
+              console.log("The unit price is: " + res[i].price);
+              console.log("The total cost of your purchase is: " + (res[i].price * answer.units));
+  
+              console.log("Your order was processed Thank you for buying with us!\n");
+  
+              // Case escenario 2: We have some units available but not the whole order. 
+            } else if (stockAvaible > 0 && answer.units > stockAvaible) {
+  
+              console.log("Sorry, Insufficient quantity! We have: " + stockAvaible + " Units of the product available");
+            }
+            else {
+              // Case escenario 2: We dont have anything. 
+              console.log("Sorry, Insufficient quantity!");
+            }
+            // console.log((res[i].stock_quantity) - answer.units);\\
+            secondMenu();
+          }
+  
+        });
+  
+  
+      });
+  
+  }
